66a8efd25579f61f63ac83fe4b60232e9d1d1ce5
Rayssa Smaniotto <ray.smaniot1@gmail.com>
2017-08-17 17:23:52 -0300

    Desenvolvimento da classe Revista com atributos

--- src/gitInit.java
+++ src/gitInit.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+git init := incializa o repositório
 
-/**
- *
- * @author 98971
- */
-public class gitInit {
-    
-}
+git status := 
+        caso vermelho não será enviado ao repositório
+        caso verde será enviado ao repositório
+
+git add . := adiciona todos os itens para serem enviados ao repositório
+
+git remote add origin link := dizendo para onde será enviado o projeto
+
+git commit -m :=  "MENSAGEM DESCREVENDO O QUE FOI FEITO"
+
+git push := envia as alterações para o repositório
